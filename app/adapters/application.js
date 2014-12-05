aa36207b82e45c3136aca29013364dc61167cb2f
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2014-12-05 21:29:11 +1100

    https://github.com/simplabs/ember-cli-simple-auth/issues/21#issuecomment-57145538

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-import ENV from 'ember-cli-ember-fire/config/environment';
+import ENV from '../config/environment';
 
 export default DS.FirebaseAdapter.extend({
   firebase: new window.Firebase('https://' + ENV.firebase_instance + '.firebaseio.com')
